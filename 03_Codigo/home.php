c63c4a3a90653aa6506afc8b0a0cc0d199a345e4
Natani Gayardo <99851402+Nati-i@users.noreply.github.com>
2025-07-03 00:51:18 -0300

    Create home.php
    código para home.php

---
+++ 03_Codigo/home.php
@@ -0,0 +1,7 @@
+<!-- interface/home.php -->
+<?php include 'header.php'; ?>
+
+<h2>Bem-vindo ao Sistema de Reservas</h2>
+<p>Use o menu para gerenciar suas reservas.</p>
+
+<?php include 'footer.php'; ?>
